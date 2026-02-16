527ae6ca445f781ad3eeee7641f07505232f0fad
IMmercato <imesh.fernando100@gmail.com>
2026-02-16 12:04:25 +0100

    Gradle Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
+    }
+
     buildFeatures {
         compose = true
     }
